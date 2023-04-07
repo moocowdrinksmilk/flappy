b72c302b893bad26ff0dedc0b8506e66ce3844c0
Rui Xian <tayrx9988@gmail.com>
2023-04-08 00:23:47 +0800

    no lines

--- android/src/com/zain/game/AndroidLauncher.java
+++ android/src/com/zain/game/AndroidLauncher.java
@@ -11,7 +11,5 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new FlappyBird(), config);
-
-
 	}
 }
